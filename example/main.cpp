d009167747ac7d9e50bc6a8d112e898df5d3c8cb
Dmitry Toropchin <dmitry.toropchin@gmail.com>
2014-09-26 18:19:32 +0400

    более подробный пример использования виджета

--- example/main.cpp
+++ example/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <datepicker/datepicker.h>
+#include <datepicker/datepickerhumanreadableformater.h>
 
 
 int main(int argc, char *argv[])
@@ -9,8 +10,15 @@ int main(int argc, char *argv[])
     init_datepicker(&app);
 
     DatePicker picker;
-    picker.setAllowedPickerTypes(PeriodType | DayType);
+
+    picker.setAllowedPickerTypes(PeriodType | DayType);         // is default
+    picker.setEditable(true);                                   // is default
+    picker.setFormater(new DatePickerHumanReadableFormater());  // is default (formater will be deleted in picker destructor)
+
+    Q_UNUSED(picker.label());                                   // date label can be customized in usual way
+
     picker.setDate(QDate::currentDate());
+
     picker.show();
 
     return app.exec();
