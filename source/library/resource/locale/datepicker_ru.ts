56f08f764cd9c644b6c1f44eef8a3b910ed5ffba
Dmitry Toropchin <dmitry.toropchin@gmail.com>
2014-10-02 10:57:28 +0400

    доделаны переводы

--- source/library/resource/locale/datepicker_ru.ts
+++ source/library/resource/locale/datepicker_ru.ts
@@ -14,106 +14,138 @@
         <translation>период</translation>
     </message>
     <message>
-        <location filename="../../source/datepicker.cpp" line="54"/>
-        <location filename="../../source/datepickerpopup.cpp" line="61"/>
+        <location filename="../../source/datepicker.cpp" line="57"/>
+        <location filename="../../source/datepickerpopup.cpp" line="73"/>
         <source>Date Picker</source>
         <translation>Выбор даты</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="19"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="23"/>
         <source>january</source>
         <translation>января</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="20"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="24"/>
         <source>february</source>
         <translation>февраля</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="21"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="25"/>
         <source>march</source>
         <translation>марта</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="22"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="26"/>
         <source>april</source>
         <translation>апреля</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="23"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="27"/>
         <source>may</source>
         <translation>мая</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="24"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="28"/>
         <source>june</source>
         <translation>июня</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="25"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="29"/>
         <source>july</source>
         <translation>июля</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="26"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="30"/>
         <source>august</source>
         <translation>августа</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="27"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="31"/>
         <source>september</source>
         <translation>сентября</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="28"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="32"/>
         <source>october</source>
         <translation>октября</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="29"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="33"/>
         <source>november</source>
         <translation>ноября</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="30"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="34"/>
         <source>december</source>
         <translation>декабря</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="32"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="36"/>
         <source>from</source>
+        <comment>date</comment>
         <translation>с</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="33"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="37"/>
         <source>to</source>
+        <comment>date</comment>
         <translation>по</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="60"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="39"/>
+        <source>from</source>
+        <comment>time</comment>
+        <translation>с</translation>
+    </message>
+    <message>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="40"/>
+        <source>to</source>
+        <comment>time</comment>
+        <translation>до</translation>
+    </message>
+    <message>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="145"/>
+        <source>%1 %2 %3 %4 %5</source>
+        <translation>%1 %2 %3 %4 %5</translation>
+    </message>
+    <message>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="158"/>
+        <source>%1 %2</source>
+        <translation>%1 %2</translation>
+    </message>
+    <message>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="173"/>
+        <source>%1 %2 %3 %4 %5 %6</source>
+        <translation>%1 %2 %3 %4 %5 %6</translation>
+    </message>
+    <message>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="67"/>
         <source>today</source>
         <translation>сегодня</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="63"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="70"/>
         <source>yesterday</source>
         <translation>вчера</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="66"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="73"/>
         <source>tomorrow</source>
         <translation>завтра</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="69"/>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="96"/>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="102"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="76"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="106"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="112"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="140"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="163"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="168"/>
         <source>%1 %2 %3</source>
         <oldsource>%1 %2 %3 year</oldsource>
         <translation>%1 %2 %3</translation>
     </message>
     <message>
-        <location filename="../../source/datepickerhumanreadableformater.cpp" line="107"/>
+        <location filename="../../source/datepickerhumanreadableformater.cpp" line="117"/>
         <source>%1 %2 %3 %4</source>
         <translation>%1 %2 %3 %4</translation>
     </message>
